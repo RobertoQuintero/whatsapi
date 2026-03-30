b5a1f96edacf1b6a81ff688f79c1a56d036caa73
Roberto Quintero <langdonhp435@gmail.com>
2026-03-29 20:47:09 -0600

    modifica metodo verifyToken

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,8 +1,28 @@
+
 export const verifyToken = (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'Token verified successfully'
-    });
+
+    try {
+
+        const accessToken= "OUYOIUYOIUYOUYUYXUYCVXUYVISUDFYISUDY"
+        const token=req.query["hub.verify_token"];
+        const challenge=req.query["hub.challenge"];
+
+        if(challenge !=null && token !==null && token === accessToken){
+            res.status(200).send(challenge);
+        }else{
+            res.status(403).json({
+                status: 'error',
+                message: 'Invalid token'
+            });
+        }
+        
+        
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: 'Token verification failed'
+        });
+    }
 }
 
 export const receivedMessage = (req, res) => {
